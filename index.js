1bf54dd054fdfacdfcc94f392f2c25ae296220b1
BatraAayush <67547222+BatraAayush@users.noreply.github.com>
2024-09-23 13:51:14 +0530

    add cors correctly

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ require("./db/db.connect");
 const express = require("express");
 const authRoute = require("./routes/auth.router");
 const employeeRoute = require("./routes/employee.router");
-// const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-// app.use(cors);
+app.use(cors());
 
 app.use("/api", authRoute);
 app.use("/api", employeeRoute);
